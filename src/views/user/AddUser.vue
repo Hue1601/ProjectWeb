fdda4987c99a031f70e175471c2328466abdf3d8
Hue1601 <phamthihue3340@gmail.com>
2024-08-29 15:49:05 +0700

    thêm validate

--- src/views/user/AddUser.vue
+++ src/views/user/AddUser.vue
@@ -2,20 +2,26 @@
   <h2 style="text-align: center; margin-top: 5px">Thêm mới nhân viên</h2>
   <div class="container">
     <label class="form-label">Username</label>
-    <input type="text" class="form-control" v-model="users.username" />
+    <input type="text" class="form-control" v-model="users.username" v-bind:class="{'is-invalid':error.username}"/>
+    <div class="invalid-feedback">{{error.username}}</div>
 
     <label class="form-label">Password</label>
-    <input type="password" class="form-control" v-model="users.pass" />
+    <input type="password" class="form-control" v-model="users.pass"  v-bind:class="{'is-invalid':error.pass}" />
+     <div class="invalid-feedback">{{error.pass}}</div>
 
     <label class="form-label">SDT</label>
-    <input type="text" class="form-control" v-model="users.sdt" />
+    <input type="text" class="form-control" v-model="users.sdt" v-bind:class="{'is-invalid':error.sdt}" />
+     <div class="invalid-feedback">{{error.sdt}}</div>
 
     <label class="form-label">Địa chỉ</label>
-    <input type="text" class="form-control" v-model="users.diachi" />
+    <input type="text" class="form-control" v-model="users.diachi" v-bind:class="{'is-invalid':error.diachi}"/>
+    <div class="invalid-feedback">{{error.diachi}}</div>
 
     <label class="form-label">Giới tính</label>
     <!-- <input type="text" class="form-control" v-model="users.gioitinh" /> -->
-    <select class="form-select" v-model="users.gioitinh" aria-label="Default select example">
+    <select class="form-select" v-model="users.gioitinh" aria-label="Default select example" v-bind:class="{'is-invalid':error.gioitinh}">
+       <div class="invalid-feedback">{{error.gioitinh}}</div>
+
    <option disabled value="">Chọn giới tính</option>
    <option value="Nam">Nam</option>
    <option value="Nữ">Nữ</option>
@@ -34,6 +40,13 @@ export default {
   name: "add-user",
   data() {
     return {
+      error:{
+          username: "",
+        pass: "",
+        sdt: "",
+        diachi: "",
+        gioitinh: "",
+      },
       users: {
         username: "",
         pass: "",
@@ -44,15 +57,43 @@ export default {
     };
   },
   methods: {
+    async validate(){
+       this.error={
+          username: "",
+        pass: "",
+        sdt: "",
+        diachi: "",
+        gioitinh: "",
+      }
+         if(!this.users.username){
+          this.error.username="Please fill in here"
+         }
+         if(!this.users.pass){
+          this.error.pass="Please fill in here"
+         }
+         if(!this.users.sdt){
+          this.error.sdt="Please fill in here"
+         }
+         if(!this.users.diachi){
+          this.error.diachi="Please fill in here"
+         }
+         if(!this.users.gioitinh){
+          this.error.gioitinh="Please fill in here"
+         }
+    },
     async AddUser() {
       try {
-        if (confirm("Are you sure you want to add this user?")) {
+        if(this.validate()){
+        // if (confirm("Are you sure you want to add this user?")) {
           //await axios.post(baseUrl, this.users);
           await axios.post(`${baseUrl}/add-user`, this.users);
-          alert("User added successfully!");
+          // alert("User added successfully!");
           this.$router.push("/user");
           this.resetForm();
+        // }
         }
+        
+        
       } catch (error) {
         console.error(error);
       }
--- src/views/user/UpdateUser.vue
+++ src/views/user/UpdateUser.vue
@@ -3,24 +3,31 @@
     <h2 style="text-align:center;margin-top:5px">Update thông tin nhân viên</h2>
     <form @submit.prevent="UpdateUser">
       <div class="container">
-        <label class="form-label">Username</label>
-        <input type="text" class="form-control" v-model="users.username" />
+   <label class="form-label">Username</label>
+    <input type="text" class="form-control" v-model="users.username" v-bind:class="{'is-invalid':error.username}"/>
+    <div class="invalid-feedback">{{error.username}}</div>
 
-        <label class="form-label">Password</label>
-        <input type="password" class="form-control" v-model="users.pass" />
+    <label class="form-label">Password</label>
+    <input type="password" class="form-control" v-model="users.pass"  v-bind:class="{'is-invalid':error.pass}" />
+     <div class="invalid-feedback">{{error.pass}}</div>
 
-        <label class="form-label">SDT</label>
-        <input type="text" class="form-control" v-model="users.sdt" />
-        
-        <label class="form-label">Địa chỉ</label>
-        <input type="text" class="form-control" v-model="users.diachi" />
-        
-        <label class="form-label">Giới tính</label>
-        <!-- <input type="text" class="form-control" v-model="users.gioitinh" /> -->
-         <select class="form-select" v-model="users.gioitinh" aria-label="Default select example">
-           <option value="Nam">Nam</option>
-           <option value="Nữ">Nữ</option>
-         </select>
+    <label class="form-label">SDT</label>
+    <input type="text" class="form-control" v-model="users.sdt" v-bind:class="{'is-invalid':error.sdt}" />
+     <div class="invalid-feedback">{{error.sdt}}</div>
+
+    <label class="form-label">Địa chỉ</label>
+    <input type="text" class="form-control" v-model="users.diachi" v-bind:class="{'is-invalid':error.diachi}"/>
+    <div class="invalid-feedback">{{error.diachi}}</div>
+
+    <label class="form-label">Giới tính</label>
+    <!-- <input type="text" class="form-control" v-model="users.gioitinh" /> -->
+    <select class="form-select" v-model="users.gioitinh" aria-label="Default select example" v-bind:class="{'is-invalid':error.gioitinh}">
+       <div class="invalid-feedback">{{error.gioitinh}}</div>
+
+   <option disabled value="">Chọn giới tính</option>
+   <option value="Nam">Nam</option>
+   <option value="Nữ">Nữ</option>
+   </select>
         <button type="submit" class="btn btn-success" style="margin-top:5px; margin-left:45%">Update</button>
       </div>
     </form>
@@ -35,6 +42,13 @@ export default {
   name: 'update-user',
   data() {
     return {
+       error:{
+          username: "",
+        pass: "",
+        sdt: "",
+        diachi: "",
+        gioitinh: "",
+      },
       users: {
         id: null,
         username: "",
@@ -60,8 +74,33 @@ export default {
         console.error(error);
       }
     },
+      async validate(){
+       this.error={
+          username: "",
+        pass: "",
+        sdt: "",
+        diachi: "",
+        gioitinh: "",
+      }
+         if(!this.users.username){
+          this.error.username="Please fill in here"
+         }
+         if(!this.users.pass){
+          this.error.pass="Please fill in here"
+         }
+         if(!this.users.sdt){
+          this.error.sdt="Please fill in here"
+         }
+         if(!this.users.diachi){
+          this.error.diachi="Please fill in here"
+         }
+         if(!this.users.gioitinh){
+          this.error.gioitinh="Please fill in here"
+         }
+    },
     async UpdateUser() {
-      if (confirm('Are you sure you want to update this user?')) {
+      // if (confirm('Are you sure you want to update this user?')) {
+        if (this.validate()) {
         try {
           //await axios.put(`${baseUrl}/${this.users.id}`, this.users);
           await axios.put(`${baseUrl}/update-user/${this.users.id}`, this.users);
@@ -69,7 +108,7 @@ export default {
           this.resetForm(); 
         } catch (error) {
           console.error(error);
-          alert('An error occurred while updating the user.');
+         
         }
       }
     },
